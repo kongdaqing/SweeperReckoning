290b94f115a297afbe574249931af43f958bbe93
zhangxing <zhangxing@zerozero.cn>
2019-12-06 19:55:55 +0800

    删除output文件夹，输出直接在data中

--- output/lpfData.m
+++
@@ -1,9 +0,0 @@
-subplot(3,1,1)
-plot(att.t,att.ax,'g',att.t,att.lax,'r');
-grid on;
-subplot(3,1,2)
-plot(att.t,att.ay,'g',att.t,att.lay,'r');
-grid on;
-subplot(3,1,3)
-plot(att.t,att.az,'g',att.t,att.laz,'r');
-grid on;
