050f0c707c6540609a8df3543a59c43e0bca5c6d
zhangxing <zhangxing@zerozero.cn>
2019-12-19 20:44:01 +0800

    same as *.cpp

--- src/AnomalyDetector/anomalyDetector.hpp
+++ src/AnomalyDetector/anomalyDetector.hpp
@@ -1,19 +1,58 @@
 #ifndef ANOMALYDETECTOR_H
 #define ANOMALYDETECTOR_H
-
 #include "../Sensor/sensorType.hpp"
+#include "../Sensor/imu.hpp"
+#include "../Sensor/odometry.hpp"
+#include "../Sensor/optFlow.hpp"
+#include <unistd.h>
 #include <fstream>
 using namespace std;
+
+enum DetectWrongType
+{
+  good,
+  wheelNoTwistSlip,
+  wheelTwistSlip,
+  optflowFaliure
+};
+struct DetectType
+{
+  DetectType()
+  {
+    Clear();
+    robotStatus =DetectWrongType::good;
+  };
+  void Clear()
+  {
+    posFopt.setZero();
+    posFodom.setZero();
+    count = 0;
+  };
+  Eigen::Vector3d posFodom;
+  Eigen::Vector3d posFopt;
+  int count;
+  int robotStatus;
+};
 
 class AnomalyDetector{
 public:
 AnomalyDetector(string _configFile);
+AnomalyDetector(string _configFile,IMU *_imu,Odometry* _odom,OptFlow *_opt);
+void Initialize();
 ~AnomalyDetector();
-bool DetectOdomOptState(const IMUType& _imu,const OptFlowType& _opt,const OdometryType& _odom);
-
-
+void DetectOdomOptState(const IMUType& _imu,const OptFlowType& _opt,const OdometryType& _odom);
+int GetRobotState(){return detectFactor.robotStatus;};
 private:
+bool getGyroBiasFlg;
+string configFile;
 ofstream recordFile;
+Odometry * odomPtr;
+OptFlow * optPtr;
+IMU * imuPtr;
+int detectPeriodNum;
+double detectDPosThreshold;
+DetectType detectFactor;
+
 
 };
 
